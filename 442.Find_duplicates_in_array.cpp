95c4fc42b36cd160abf06945aae5d3aedaf13c94
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-03-31 21:29:29 +0530

    442

--- 442.Find_duplicates_in_array.cpp
+++ 442.Find_duplicates_in_array.cpp
@@ -13,12 +13,13 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         if(nums.empty())return {};
         vector<int>ans;
-        if(nums[abs(nums[i])-1] < 0){
-            ans.push_back(abs(nums[i]));    
+        for(int i=0;i<nums.size();i++){
+            if(nums[abs(nums[i])-1]<0)
+                ans.push_back(abs(nums[i]));
+            nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
         }
-        nums[abs(nums[i])-1] = -nums[abs(nums[i])-1];
+        return ans;
     }
-    return ans;
 };
 
 // Time Complexcity - O(N)
