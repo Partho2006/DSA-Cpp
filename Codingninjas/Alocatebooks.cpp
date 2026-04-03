ce4b468e83f8015e1236bb9668ded34bda1e6ab8
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-04-04 00:08:42 +0530

    Alocate books

---
+++ Codingninjas/Alocatebooks.cpp
@@ -0,0 +1,75 @@
+// Q. Allocate Books
+
+// Given an array ‘arr’ of integer numbers, ‘arr[i]’ represents the number of pages in the ‘i-th’ book.
+
+// There are ‘m’ number of students, and the task is to allocate all the books to the students.
+
+// Allocate books in such a way that:
+
+// 1. Each student gets at least one book.
+// 2. Each book should be allocated to only one student.
+// 3. Book allocation should be in a contiguous manner.
+
+// You have to allocate the book to ‘m’ students such that the maximum number of pages assigned to a student is minimum.
+// If the allocation of books is not possible, return -1.
+
+// Example:
+// Input: ‘n’ = 4 ‘m’ = 2 
+// ‘arr’ = [12, 34, 67, 90]
+
+// Output: 113
+
+#include<vector>
+using namespace std;
+
+bool isPossible(vector<int> arr, int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+    //cout << "checking for mid "<< mid <<endl;
+    
+    for(int i = 0; i<n; i++) {
+        if(pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+        else
+        {
+            studentCount++;
+            if(studentCount > m || arr[i] > mid ) {
+            return false;
+        }
+            pageSum = arr[i];
+        }
+        if(studentCount > m) {
+            return false;
+        }
+        //cout << " for i " << i << " Student "<< studentCount << " pageSum " << pageSum << endl;
+    }
+    return true;
+}
+
+int allocateBooks(vector<int> arr, int n, int m) {
+    int s = 0;
+    int sum = 0;
+    
+    for(int i = 0; i<n; i++) {
+        sum += arr[i];
+    }
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    
+    while(s<=e)
+    {
+        if(isPossible(arr,n,m,mid)) {
+            //cout<<" Mid returned TRUE" << endl;
+            ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
