164324962b359c9581b5de061bc0512a554fb24a
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-04-15 17:13:12 +0530

    204

---
+++ 204.Count_prime.cpp
@@ -0,0 +1,32 @@
+// 204. Count Primes
+
+// Given an integer n, return the number of prime numbers that are strictly less than n.
+
+// Example 1:
+// Input: n = 10
+// Output: 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        int c=0;
+        vector<bool> prime(n+1, true);
+        
+        prime[0]=prime[1]=0;
+
+        for(int i=2; i<n; i++){
+            if(prime[i]){
+                c++;
+
+                for(int j=2*i; j<n; j=j+i){
+                    prime[j]=0;
+                }
+            }
+        }
+        return c;
+    }
+};
+
+// Time complexity: O(nloglogn)
+// Space complexity: O(n)
