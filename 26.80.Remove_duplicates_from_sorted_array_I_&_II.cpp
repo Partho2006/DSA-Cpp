0e6c29b579e12fdc7ac69575bbe36526c58ec8ed
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-04-17 20:41:11 +0530

    80

--- 26.80.Remove_duplicates_from_sorted_array_I_&_II.cpp
+++ 26.80.Remove_duplicates_from_sorted_array_I_&_II.cpp
@@ -16,3 +16,27 @@ public:
         return index;
     }
 };
+
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+
+// 80. Remove Duplicates from Sorted Array II
+
+// Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+
+        if (nums.size() <= 2) return nums.size();
+        
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;       
+    }
+};  
