741945f2d2c496be650f314f33746d530470704b
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-04-20 16:12:05 +0530

    55

--- 55.45.Jump_game_I-&-II.cpp
+++ 55.45.Jump_game_I-&-II.cpp
@@ -23,3 +23,44 @@ public:
         return goal == 0;        
     }
 };
+ 
+____________________________________________________________________________________________________________________
+
+// 45. Jump Game II
+
+// You are given a 0-indexed array of integers nums of length n. You are initially positioned at index 0.
+
+// Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at index i, you can jump to any index (i + j) where:
+
+// 0 <= j <= nums[i] and
+// i + j < n
+// Return the minimum number of jumps to reach index n - 1. The test cases are generated such that you can reach index n - 1.
+
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: 2
+
+class Solution {
+ public:
+  int jump(vector<int>& nums) {
+    int ans = 0;
+    int end = 0;
+    int farthest = 0;
+
+    // Implicit BFS
+    for (int i = 0; i < nums.size() - 1; ++i) {
+      farthest = max(farthest, i + nums[i]);
+      if (farthest >= nums.size() - 1) {
+        ++ans;
+        break;
+      }
+      if (i == end) {    // Visited all the items on the current level
+        ++ans;           // Increment the level
+        end = farthest;  // Make the queue size for the next level
+      }
+    }
+
+    return ans;
+  }
+};
+
